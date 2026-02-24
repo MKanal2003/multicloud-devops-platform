9faf1f7afdaaa76ad26a5fb3240457e9174ef002
Mallikarjuna Kanal <mkanal2023@gmail.com>
2026-02-24 22:38:51 +0530

    Some lines are updated....

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Hello 👋 Mallikarjuna! Your DevOps Multi-Cloud App is Running\n");
+    res.end("Hello 👋 MK! Version 2 deployed automatically 🚀\n");
+    res.end("Hello 👋 Welcome to the Multi-Cloud App! 🚀\n");
 });
 
 server.listen(PORT, () => {
