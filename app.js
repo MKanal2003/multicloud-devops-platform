669cb870a7f235286d275c150af3ef3a598a6b44
Mallikarjuna Kanal <mkanal2023@gmail.com>
2026-02-28 14:17:44 +0530

    Helath check

--- app.js
+++ app.js
@@ -1,46 +1,21 @@
 const http = require("http");
 
 const PORT = 3000;
-const VERSION = process.env.VERSION || "DEV";
-const HOSTNAME = require("os").hostname();
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
 
-    res.end(`
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
-    <!-- Link to external CSS file for styling (optional) -->
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="login-container">
-        <form action="/login_endpoint" method="POST">
-            <h2>Mallikarjuna Kanal</h2>
-            <div class="input-group">
-                <label for="username">Username</label>
-                <input type="text" id="username" name="username" required>
-            </div>
-            <div class="input-group">
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password" required>
-            </div>
-            <button type="submit">Sign In</button>
-            <div class="options">
-                <a href="/forgot_password">Forgot Password?</a>
-            </div>
-        </form>
-    </div>
-</body>
-</html>
+    if (req.url === "/health") {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("OK");
+        return;
+    }
 
-`);
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("Multi-Cloud DevOps Platform Running 🚀\n");
+    res.write("Automatic Deployment Active ✅\n");
+    res.end();
 });
 
 server.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
+    console.log(`Server running on port ${PORT}`);
 });
