c63fcb87d694defc3aa93cf6ef36f59aa9014b64
Mallikarjuna Kanal <mkanal2023@gmail.com>
2026-02-28 22:54:41 +0530

    load test

--- app.js
+++ app.js
@@ -2,18 +2,37 @@ const http = require("http");
 
 const PORT = 3000;
 
+let ready = false;
+
+// simulate app boot time (database, cache, etc)
+setTimeout(() => {
+    ready = true;
+    console.log("Application READY");
+}, 15000); // 15 seconds warmup
+
 const server = http.createServer((req, res) => {
 
+    // Health check (server alive)
     if (req.url === "/health") {
-        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.writeHead(200);
         res.end("OK");
         return;
     }
 
+    // Readiness check (server ready to receive traffic)
+    if (req.url === "/ready") {
+        if (ready) {
+            res.writeHead(200);
+            res.end("READY");
+        } else {
+            res.writeHead(503);
+            res.end("NOT READY");
+        }
+        return;
+    }
+
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.write("Multi-Cloud DevOps Platform Running 🚀\n");
-    res.write("Automatic Deployment Active witth mkkkkkkkk\n");
-    res.end();
+    res.end("Multi-Cloud DevOps Platform Running 🚀\nZero Downtime Deployment Active");
 });
 
 server.listen(PORT, () => {
